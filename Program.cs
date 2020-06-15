68a6730c86220b12a567cdc4cbf09c9ddbb262a4
saudiretard <66959704+saudiretard@users.noreply.github.com>
2020-06-15 18:50:05 +0400

    some additions
    aa

--- Program.cs
+++ Program.cs
@@ -3,10 +3,13 @@ TIC TAC TOE MADE BY kevz#2073
 MADE WITH C# WITH AI [NO MINIMAX ALGORITHM]
 This AI may be unbeatable
 
+This game is fun + multiplayer is not implemented yet.
+
 This code is a little bit mess up, since im only a beginner. :)
-Thanks to :
+Thanks (Credits) to :
 - Abood#1337 (helping me alot in C# before)
-- loanselot#1337 (idk)
+- loanselot#1337 (idk btw, he is professional coder in c#)
+- my other mates (sl*v3s)
 
 */
 
